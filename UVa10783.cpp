7a4c338084f178491827c4e99117fc6d47cdb216
jamesyea <pengyea0622@gmail.com>
2023-03-21 08:45:56 +0800

    success

--- UVa10783.cpp
+++ UVa10783.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main(){
     int x;
     cin>>x;
-    while(x--){
+    for(int cs=1;cs<=x;cs++){
         int a,b;
         cin>>a>>b;
         int sum=0;
         for(int i=a;i<=b;i++){
             if(i&1) sum+=i; 
         }
-        cout<<sum<<endl;
+        cout<<"Case "<<cs<<": "<<sum<<endl;
     }
 }
