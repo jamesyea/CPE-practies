b120f4108fa736e28c6622b17d2aed6c17464380
jamesyea <pengyea0622@gmail.com>
2023-10-29 13:53:35 +0800

    update 2023/10/29

--- CPE49/UVa10038.cpp
+++ CPE49/UVa10038.cpp
@@ -1,53 +1,34 @@
 //24
 #include<iostream>
-
+#include<cstring>
 using namespace std;
-
 int main(){
     int n;
     while(cin>>n){
-        int *cnt = new int[n];
-        int *num = new int[n];
-        for(int i=0; i<n; i++)
-        {
-            cin>>num[i];
-            cnt[i]=0;
+        int arr[n];
+        bool check[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
         }
-        if(n==1)
-        {
+        memset(check,false,sizeof(check));
+        if(n==1){
             cout<<"Jolly"<<endl;
-        }
-        else
-        {
-            int j=0;
-            for(j=0; j<n-1; j++)
-            {
-                int diff = abs(num[j+1]-num[j]);
-                if(diff<=0 || diff>n-1)
-                {
-                    cout<<"Not jolly"<<endl;
+        }else{
+            int d;
+            bool flag=true;
+            for(int i=0;i<n-1;i++){
+                d=abs(arr[i]-arr[i+1]);
+                if(d==0 || d>=n || check[d]){
+                    flag=false;
                     break;
                 }
-                cnt[diff]+=1;
+                check[d]=true;
             }
-            if(j>=n-1)
-            {
-                int k=0;
-                for(k=1; k<n; k++)
-                {
-                    if(cnt[k]==0)
-                    {
-                        cout<<"Not jolly"<<endl;
-                        break;
-                    }
-                }
-                if(k>=n)
-                    cout<<"Jolly"<<endl;
-
+            if(flag){
+                cout<<"Jolly"<<endl;
+            }else{
+                cout<<"Not jolly"<<endl;
             }
-
         }
-
     }
-
 }
